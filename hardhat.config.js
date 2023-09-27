6b6e0d09f2fd0e92c1795885408e8ee51c03e3a5
bilalahmad9755 <115340899+bilalahmad9755@users.noreply.github.com>
2023-09-27 22:36:20 +0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -49,14 +49,14 @@ module.exports = {
   },
   networks: {
     rinkeby: {
-      url: `https://eth-rinkeby.alchemyapi.io/v2/l7dI0Klq2euD2SI3wM1FJ_B5C6ur9gQm`,
-      accounts: ["4ff4c1f54ffc9172aca57bd22fff489c79d0a5ed42e691f84926e9e93a916ba5"]
+      url: `https://eth-rinkeby.alchemyapi.io/v2/abc`,
+      accounts: ["abc"]
     },
     avalanche:
     {
       url:  `https://api.avax-test.network/ext/bc/C/rpc`,
       chainId: 43113,
-      accounts: ["4ff4c1f54ffc9172aca57bd22fff489c79d0a5ed42e691f84926e9e93a916ba5",]
+      accounts: ["abc",]
     }
   }, 
 };
